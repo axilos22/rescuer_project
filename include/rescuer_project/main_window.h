c00043187e7c3a03684a16c378d87a836cbbf71a
axilos22 <zenos_22@hotmail.com>
2016-10-28 11:17:51 +0200

    Format changes. Corrected: the wrong expression of declare class.

--- include/rescuer_project/main_window.h
+++ include/rescuer_project/main_window.h
@@ -10,20 +10,20 @@
 
 namespace rescuer_project {
 
-class MyWindow 
-  : public rqt_gui_cpp::Plugin
-  {
-	Q_OBJECT
-	public:
-		MyWindow();
-		virtual void initPlugin(qt_gui_cpp::PluginContext& context);
-		virtual void shutdownPlugin();
-		virtual void saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const;
-		virtual void restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings);
-	private:
-		QPushButton* m_button;
-		QWidget* widget_;
-		QPushButton* button_;
-	};
+class MyWindow
+        : public rqt_gui_cpp::Plugin
+{
+    Q_OBJECT
+public:
+    MyWindow();
+    virtual void initPlugin(qt_gui_cpp::PluginContext& context);
+    virtual void shutdownPlugin();
+    virtual void saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const;
+    virtual void restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings);
+private:
+    QPushButton* m_button;
+    QWidget* widget_;
+    QPushButton* button_;
+};
 }//namespace
 #endif // MAINWINDOW_H
--- src/rescuer_project/main_window.cpp
+++ src/rescuer_project/main_window.cpp
@@ -4,36 +4,36 @@
 
 namespace rescuer_project {
 MyWindow::MyWindow():rqt_gui_cpp::Plugin(),widget_(0) {
-	setObjectName("Ma fenetre");
+    setObjectName("Ma fenetre");
 }
 void MyWindow::initPlugin(qt_gui_cpp::PluginContext& context)
 {
-  // access standalone command line arguments
-  QStringList argv = context.argv();
-  // create QWidget
-  widget_ = new QWidget();
-  button_ = new QPushButton("Press me plz !!!",widget_);
-  // extend the widget with all attributes and children from UI file
-  //~ ui_.setupUi(widget_);
-  // add widget to the user interface
-  context.addWidget(widget_);
+    // access standalone command line arguments
+    QStringList argv = context.argv();
+    // create QWidget
+    widget_ = new QWidget();
+    button_ = new QPushButton("Press me plz !!!",widget_);
+    // extend the widget with all attributes and children from UI file
+    //~ ui_.setupUi(widget_);
+    // add widget to the user interface
+    context.addWidget(widget_);
 }
 
 void MyWindow::shutdownPlugin()
 {
-  // TODO unregister all publishers here
+    // TODO unregister all publishers here
 }
 
 void MyWindow::saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const
 {
-  // TODO save intrinsic configuration, usually using:
-  // instance_settings.setValue(k, v)
+    // TODO save intrinsic configuration, usually using:
+    // instance_settings.setValue(k, v)
 }
 
 void MyWindow::restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings)
 {
-  // TODO restore intrinsic configuration, usually using:
-  // v = instance_settings.value(k)
+    // TODO restore intrinsic configuration, usually using:
+    // v = instance_settings.value(k)
 }
 } // namespace
-PLUGINLIB_DECLARE_CLASS(rqt_example_cpp, MyPlugin, rescuer_project::MyWindow, rqt_gui_cpp::Plugin)
+PLUGINLIB_DECLARE_CLASS(rescuer_project, MyWindow, rescuer_project::MyWindow, rqt_gui_cpp::Plugin)
