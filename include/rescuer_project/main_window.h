c64ad451713a253a6fba939a2418bfabc3af53cf
axilos22 <zenos_22@hotmail.com>
2016-10-30 23:50:30 +0100

    The rqt GUI now compiles AND run properly.

--- include/rescuer_project/main_window.h
+++ include/rescuer_project/main_window.h
@@ -2,28 +2,24 @@
 #define MAINWINDOW_H
 
 #include <rqt_gui_cpp/plugin.h>
-//~ #include <rqt_gui/ui_my_plugin.h>
 
 #include <QWidget>
-#include <QMainWindow>
 #include <QPushButton>
 
 namespace rescuer_project {
 
-class MyWindow
-        : public rqt_gui_cpp::Plugin
-{
-    Q_OBJECT
+class Rescuer_gui
+        : public rqt_gui_cpp::Plugin {
+//    Q_OBJECT
 public:
-    MyWindow();
+    Rescuer_gui();
     virtual void initPlugin(qt_gui_cpp::PluginContext& context);
     virtual void shutdownPlugin();
     virtual void saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const;
     virtual void restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings);
 private:
-    QPushButton* m_button;
     QWidget* widget_;
-    QPushButton* button_;
+    QPushButton *_button;
 };
 }//namespace
 #endif // MAINWINDOW_H
--- src/rescuer_project/main_window.cpp
+++ src/rescuer_project/main_window.cpp
@@ -3,37 +3,42 @@
 #include <QStringList>
 
 namespace rescuer_project {
-MyWindow::MyWindow():rqt_gui_cpp::Plugin(),widget_(0) {
-    setObjectName("Ma fenetre");
+
+Rescuer_gui::Rescuer_gui():
+    rqt_gui_cpp::Plugin()
+    ,widget_(0) {
+    setObjectName("ObjectName");
 }
-void MyWindow::initPlugin(qt_gui_cpp::PluginContext& context)
+
+void Rescuer_gui::initPlugin(qt_gui_cpp::PluginContext& context)
 {
     // access standalone command line arguments
     QStringList argv = context.argv();
     // create QWidget
     widget_ = new QWidget();
-    button_ = new QPushButton("Press me plz !!!",widget_);
+    _button = new QPushButton("PRESS ME",widget_);
     // extend the widget with all attributes and children from UI file
     //~ ui_.setupUi(widget_);
     // add widget to the user interface
     context.addWidget(widget_);
 }
 
-void MyWindow::shutdownPlugin()
+void Rescuer_gui::shutdownPlugin()
 {
     // TODO unregister all publishers here
 }
 
-void MyWindow::saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const
+void Rescuer_gui::saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const
 {
     // TODO save intrinsic configuration, usually using:
     // instance_settings.setValue(k, v)
 }
 
-void MyWindow::restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings)
+void Rescuer_gui::restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings)
 {
     // TODO restore intrinsic configuration, usually using:
     // v = instance_settings.value(k)
 }
 } // namespace
-PLUGINLIB_DECLARE_CLASS(rescuer_project, MyWindow, rescuer_project::MyWindow, rqt_gui_cpp::Plugin)
+PLUGINLIB_DECLARE_CLASS(rescuer_project, Rescuer_gui, rescuer_project::Rescuer_gui, rqt_gui_cpp::Plugin)
+//PLUGINLIB_EXPORT_CLASS(rescuer_project::Rescuer_gui,rqt_gui_cpp::Plugin)
