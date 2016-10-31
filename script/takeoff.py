5ae65fb84d7f3cb9c572c4230473d16bbcdbc96d
axelUJI <axel@uji.es>
2016-10-31 11:41:42 +0100

    Updated script to control the drone

--- script/takeoff.py
+++ script/takeoff.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import rospy
+from ardrone_autonomy.msg import Navdata
 from std_msgs.msg import String
 from std_msgs.msg import Empty
 from geometry_msgs.msg import Pose2D, Twist
@@ -25,11 +26,11 @@ def land():
 def turnAround():
 	#~ geometryMsg=[[0,0,0],[0,0,1.0]]
 	t = Twist()
-	t.angular.z=1.0
+	t.angular.z=.9
 	loop=0
-	pub = rospy.Publisher("ardrone/cmd_vel",Twist, queue_size=10 )
+	pub = rospy.Publisher("/cmd_vel",Twist, queue_size=10 )
 	while not rospy.is_shutdown() and loop<5:
-		pub.publish(Empty())
+		pub.publish(t)
 		rate.sleep()
 		loop+=1
 		rospy.loginfo("[%d] Turn")
@@ -38,15 +39,23 @@ def stopNode():
 	land()
 	rospy.loginfo("Ending node")
 
+def navDataUpdate(navData):
+	bat = navData.batteryPercent
+	print("battery:%d" % bat)
+	state = navData.state
+	print("state: %d" % state)
+	rospy.sleep(.5)
+
 if __name__ == '__main__':
 	rospy.on_shutdown(stopNode)
 	try:
 		rospy.init_node('droneNode', anonymous=True)
 		rate = rospy.Rate(10) # 10hz
+		sub = rospy.Subscriber("ardrone/navdata",Navdata,navDataUpdate)
 		takeoff()
 		rospy.sleep(5)
 		turnAround()
-		rospy.sleep(10)
+		rospy.sleep(5)
 		land()
 	except rospy.ROSInterruptException:
 		pass
