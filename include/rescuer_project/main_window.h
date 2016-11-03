ce0f2b3eaaafa865dcf5ebcbe4638fdc7d86d973
axilos22 <zenos_22@hotmail.com>
2016-11-03 19:17:49 +0100

    UI -GUI now uses UI for quick prototyping. -Added slots for buttons

--- include/rescuer_project/main_window.h
+++ include/rescuer_project/main_window.h
@@ -1,34 +1,44 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
+/*ROS*/
+#include <ros/ros.h>
+#include <ros/macros.h>
+#include <std_msgs/Empty.h>
+#include <std_msgs/String.h>
+#include <image_transport/image_transport.h>
 /*RQT*/
 #include <rqt_gui_cpp/plugin.h>
-#include <ui_image_view.h>
-#include <image_transport/image_transport.h>
-#include <ros/macros.h>
+//#include <ui_image_view.h>
 /*QT*/
 #include <QWidget>
 #include <QPushButton>
 #include <QHBoxLayout>
+#include <QTextEdit>
+#include <QString>
 /*own*/
+#include <ui_main_window.h>
 #include "rescuer_project/rescuer_widget.h"
 
 namespace rescuer_project {
 
-class Rescuer_gui
-        : public rqt_gui_cpp::Plugin {
-//    uncommenting Q_OBJECT generate errors
+class MainWindow : public rqt_gui_cpp::Plugin {
+    //again Q_OBJECT add errors after few makes
 //    Q_OBJECT
 public:
-    Rescuer_gui();
+    MainWindow();
     virtual void initPlugin(qt_gui_cpp::PluginContext& context);
     virtual void shutdownPlugin();
     virtual void saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const;
     virtual void restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings);
-private:
-    QWidget* widget_;
-    QPushButton *_button;
-    QHBoxLayout *_hLayout;
-    RescuerWidget *_rWidget;
+    void log(QString msg);
+public slots:
+    void droneTakeOff();
+    void droneLand();
+protected:
+    QWidget* _centralWidget;
+    Ui::MainWindowWidget _ui;
+    ros::NodeHandle* _nh;
+    QTextEdit *_console;
 };
 }//namespace
 #endif // MAINWINDOW_H
--- src/rescuer_project/main_window.cpp
+++ src/rescuer_project/main_window.cpp
@@ -4,46 +4,69 @@
 
 namespace rescuer_project {
 
-Rescuer_gui::Rescuer_gui():
-    rqt_gui_cpp::Plugin()
-    ,widget_(0) {
-    setObjectName("ObjectName");
+MainWindow::MainWindow():rqt_gui_cpp::Plugin(),_centralWidget(0) {
+    setObjectName("MainWindow0");
 }
 
-void Rescuer_gui::initPlugin(qt_gui_cpp::PluginContext& context)
+void MainWindow::initPlugin(qt_gui_cpp::PluginContext& context)
 {
     // access standalone command line arguments
     QStringList argv = context.argv();
     // create QWidget
-    widget_ = new QWidget;
-    _button = new QPushButton("PRESS ME");
-    _hLayout = new QHBoxLayout;
-    _rWidget= new RescuerWidget;
-    _rWidget->initWidget();
-
-    widget_->setLayout(_hLayout);
-    _hLayout->addWidget(_rWidget);
-    _hLayout->addWidget(_button);
-
+    _centralWidget = new QWidget;
+    _ui.setupUi(_centralWidget);
+    _console = _ui.consoleTextEdit;
+    _console->setText("Console");
     // add widget to the user interface
-    context.addWidget(widget_);
+    context.addWidget(_centralWidget);
+    //TODO check if rosnode is set
+    //init rosnode and send nodeHandle object
+    ros::NodeHandle nh;
+    _nh = &nh;
+    //connect GUI
+    connect(_ui.droneTakeOffButton,SIGNAL(released()),this,SLOT(droneTakeOff()));
+    connect(_ui.droneLandButton,SIGNAL(released()),this,SLOT(droneLand()));
 }
 
-void Rescuer_gui::shutdownPlugin()
+void MainWindow::shutdownPlugin()
 {
     // TODO unregister all publishers here
 }
 
-void Rescuer_gui::saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const
+void MainWindow::saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const
 {
     // TODO save intrinsic configuration, usually using:
     // instance_settings.setValue(k, v)
 }
 
-void Rescuer_gui::restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings)
+void MainWindow::restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings)
 {
     // TODO restore intrinsic configuration, usually using:
     // v = instance_settings.value(k)
 }
+
+void MainWindow::log(QString msg)
+{
+    QString oldText = _console->toPlainText();
+    _console->setText(oldText+"\n>>"+msg);
+}
+
+void MainWindow::droneTakeOff()
+{
+    ROS_INFO("Drone has to take off now.");
+    log("Drone has to take off now.");
+    std_msgs::Empty emptyMsg;
+    ros::Publisher droneTakeOffPub = (*_nh).advertise<std_msgs::Empty>("/ardrone/takeoff",1);
+    droneTakeOffPub.publish(emptyMsg);
+}
+
+void MainWindow::droneLand()
+{
+    ROS_INFO("Drone has to land now.");
+    log("Drone has to land now.");
+    std_msgs::Empty emptyMsg;
+    ros::Publisher droneLandPub = (*_nh).advertise<std_msgs::Empty>("/ardrone/land",1);
+    droneLandPub.publish(emptyMsg);
+}
 } // namespace
-PLUGINLIB_EXPORT_CLASS(rescuer_project::Rescuer_gui,rqt_gui_cpp::Plugin)
+PLUGINLIB_EXPORT_CLASS(rescuer_project::MainWindow,rqt_gui_cpp::Plugin)
